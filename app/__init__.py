40575e55fd384347d058a9c6a897deae432c6ecc
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2024-03-23 19:05:46 +0100

    Modify to import correctly

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-# Import controllers
-from app.controllers import main_controller
-
-if __name__ == '__main__':
-    app.run(debug=True)
