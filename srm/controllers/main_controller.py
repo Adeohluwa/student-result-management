c347e11e53e6e2e418badb9d6c1ce94382d1dcff
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2024-03-25 14:13:15 +0100

    Routing logic for student and lecturer side

--- srm/controllers/main_controller.py
+++ srm/controllers/main_controller.py
@@ -89,114 +89,14 @@ def index():
 
     # Render the 'login.html' template for GET requests
     return render_template('login.html')
-# def index():
-#     if request.method == 'POST':
-#         if 'email' in request.form and 'password' in request.form:
-#             email = request.form['email']
-#             password = request.form['password']
-#             print(f" the pass is {password}")
-#             try:
-#                 user = auth.create_user(email=email, password=password)
-#                 flash('Signup successful!', 'success')
-#                 time.sleep(3)
-#                 return redirect(url_for('index'))
-#             except Exception as e:
-#                 error_message = str(e)
-#                 flash(f'Signup failed: {error_message}', 'danger')
-#             else:
-#                 flash('Passwords do not match', 'danger')
-
-#             # return email
-#     # password = request.form['loginPassword']
-#     # email = request.form['email']
-#     # password = request.form['password']
-#     # return email
-#     # return 
-
-#     # Render the 'base.html' template
-#     return render_template('login.html')
-
-# @app.route('/signup', methods=['GET','POST'])
-# def signup():
-#     # Render the 'base.html' template
-#     email = request.form['email']
-#     password = request.form['password']
-#     try:
-#         flash('Signup successful!', 'success')
-#         user = auth.create_user_with_email_and_password(email, password)
-#         return redirect(url_for('index'))
-#     except Exception as e:
-#         error_message = str(e)
-#         flash(f'Signup failed: {error_message}', 'danger')
-#     else:
-#         flash('Passwords do not match', 'danger')
-#     print(email)
-#     email = request.form['email']
-#     password = request.form['password']
-#     return f"{email}"
-#     print(password)
-#     return render_template('login.html')
 
 
+@app.route('/studentDashboard')
+def studentDashboard():
+    # Render the template with the results
+    return render_template('student_dashboard.html', courses=courses, students=students)
 
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     if request.method == 'POST':
-#         if 'loginEmail' in request.form and 'loginPassword' in request.form:
-#             email = request.form['loginEmail']
-#             password = request.form['loginPassword']
-#             try:
-#                 user = auth.sign_in_with_email_and_password(email, password)
-#                 flash('Login successful!', 'success')
-#                 return redirect(url_for('index'))
-#             except Exception as e:
-#                 error_message = str(e)
-#                 flash(f'Login failed: {error_message}', 'danger')
-#         elif 'signupEmail' in request.form and 'signupPassword' in request.form and 'signupConfirmPassword' in request.form:
-#             email = request.form['signupEmail']
-#             password = request.form['signupPassword']
-#             confirm_password = request.form['signupConfirmPassword']
-#             if password == confirm_password:
-#                 try:
-#                     user = auth.create_user_with_email_and_password(email, password)
-#                     flash('Signup successful!', 'success')
-#                     return redirect(url_for('index'))
-#                 except Exception as e:
-#                     error_message = str(e)
-#                     flash(f'Signup failed: {error_message}', 'danger')
-#             else:
-#                 flash('Passwords do not match', 'danger')
-#     return render_template('index.html')
-
-
-
-
-
-
-
-
-# @app.route('/student_results')
-# def student_results():
-#     return render_template('student_results.html', student_results=results)
-
-
-# @app.route('/student_information')
-# def student_information():
-#     return render_template('student_information.html', students=students)
-
-
-# @app.route('/course_management')
-# def course_management():
-#     return render_template('course_management.html', courses=courses)
-
-
-
-# @app.route('/lecturer_management')
-# def lecturer_management():
-#     return render_template('lecturer_management.html', lecturers=lecturers)
-
-
-
-# @app.route('/dispute')
-# def result_dispute():
-#     return render_template('result_dispute.html', result_disputes=result_disputes)
+@app.route('/lecturerDashboard')
+def lecturerDashboard():
+    # Render the template with the results
+    return render_template('lecturer_dashboard.html', courses=courses, students=students)
